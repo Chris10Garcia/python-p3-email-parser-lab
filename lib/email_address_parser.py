7093ef67bcaaf386e23fda57add53b30af313882
Christian Garcia <bones192nd@gmail.com>
2023-06-17 08:37:40 -0400

    partially complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class EmailAddressParser:
+    def __init__(self, email_list):
+        self.email_list = email_list
+
+    def parse(self):
+        return self.email_list
+    
+
+    # do regex stuff here
