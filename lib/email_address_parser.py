44d403b400be96af089c1655b9c2c1bd60a733a6
Christian Garcia <bones192nd@gmail.com>
2023-06-17 15:15:10 -0400

    fun lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,9 +1,21 @@
+import re
+
 class EmailAddressParser:
+    email_list = None
     def __init__(self, email_list):
+        if not email_list:
+            self.email_list=[]
         self.email_list = email_list
 
     def parse(self):
+        self.regex_stuff()
+        print(self.email_list)
         return self.email_list
     
 
-    # do regex stuff here
+    def regex_stuff(self):
+        pattern = r"[\w.]+@[\w]+\.[\w]+"
+        # pattern = r"\s|\,"
+        regex = re.compile(pattern)
+        self.email_list = regex.findall(self.email_list)
+        self.email_list = sorted(self.email_list)
